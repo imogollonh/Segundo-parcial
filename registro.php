c3dd16cd07d0b24473ab2dd7fc7377721c81fd14
Unknown <imogollonh@miumg.edu.gt>
2019-10-09 07:47:27 -0600

    Curriculum
    Final

---
+++ registro.php
@@ -0,0 +1,41 @@
+<?php
+$db_host="localhost";
+$db_user="root";
+$db_password="ivan";
+$db_name="prueba";
+$db_table_name="contacto";
+   $db_connection = mysql_connect($db_host, $db_user, $db_password);
+
+if (!$db_connection) {
+	die('No se ha podido conectar a la base de datos');
+}
+$subs_name = utf8_decode($_POST['nombre']);
+$subs_mensaje = utf8_decode($_POST['mensaje']);
+$subs_email = utf8_decode($_POST['email']);
+
+$resultado=mysql_query("SELECT * FROM ".$db_table_name." WHERE Email = '".$subs_email."'", $db_connection);
+
+if (mysql_num_rows($resultado)>0)
+{
+
+header('Location: Fail.html');
+
+} else {
+	
+	$insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`Nombre` , `email` , `mensaje`) VALUES ("' . $subs_name . '", "' . $subs_email . '", "' . $subs_mensaje . '")';
+
+mysql_select_db($db_name, $db_connection);
+$retry_value = mysql_query($insert_value, $db_connection);
+
+if (!$retry_value) {
+   die('Error: ' . mysql_error());
+}
+	
+header('Location: Success.html');
+
+}
+
+mysql_close($db_connection);
+
+		
+?>
